54ee93756b51ba43c9acf0296673a9f8e7339625
devora-a <aravinddharla6@gmail.com>
2026-05-27 18:32:59 +0200

    conversation

--- 03.variables.sh
+++ 03.variables.sh
@@ -8,5 +8,6 @@ echo "PERSON2: Hello $PERSON1"
 echo "PERSON1: Is that you, $PERSON2?"
 echo "PERSON2: yes mr. $PERSON1, it's me"
 echo "PERSON1: were are you gone these many days mr. $PERSON2?"
-echo "PERSON2: I went an international trip
+echo "PERSON2: I went an international trip"
+
 
