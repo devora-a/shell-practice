f0418cd0277385a2a590e7579de39aa5ae29599c
devora-a <aravinddharla6@gmail.com>
2026-06-29 16:02:17 +0200

    doc.add

--- 09-logs-archieve.sh
+++ 09-logs-archieve.sh
@@ -4,7 +4,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3: - 14}
 
-if [ -z  $SOURCE_DIR ] || [ -Z $DEST_DIR ]; then
+if [ -Z  $SOURCE_DIR ] || [ -Z $DEST_DIR ]; then
    echo "Either source directory or destination directory empty"
    echo "USAGE:: $0 [source_dir] [dest_dir] [days: default +14]"
    exit 1
