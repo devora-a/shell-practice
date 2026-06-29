e98d60977096b7c2219f0531da592fbb16616978
devora-a <aravinddharla6@gmail.com>
2026-06-29 15:51:24 +0200

    doc.add

--- 09-logs-archieve.sh
+++ 09-logs-archieve.sh
@@ -4,25 +4,25 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3: - 14}
 
-if [-z $SOURCE_DIR ] || [-Z $DEST_DIR]; then
+if [ -z  $SOURCE_DIR ] || [ -Z $DEST_DIR ]; then
    echo "Either source directory or destination directory empty"
    echo "USAGE:: $0 [source_dir] [dest_dir] [days: default +14]"
    exit 1
 fi
 
-if [! -d $SOURCE_DIR]; then
+if [ ! -d $SOURCE_DIR ]; then
    echo "source directory: $SOURCE_DIR doest not exist"
    exit 1
 fi
 
-if [! -d $DEST_DIR]; then
+if [ ! -d $DEST_DIR ]; then
    echo "destation directory: $DEST_DIR doest not exit"
    exit 1
 fi
 
-FILES=$( find $SOURCE_DIR -type f -name "*.log" -mtime $DAYS )
+FILES=$( find $SOURCE_DIR -type f -name "*.log" -mtime +$DAYS )
 
-if [-z $FILES ]; then
+if [ -Z $FILES ]; then
    echo "Logs files older then 14 days not found nothing to do"
    exit 0
 fi
