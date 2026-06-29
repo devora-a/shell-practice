d3bde57d1d60a83bb8abba6f89bd9ea581c3d5c4
devora-a <aravinddharla6@gmail.com>
2026-06-29 22:01:19 +0200

    doc.add

--- mail.sh
+++ mail.sh
@@ -7,12 +7,13 @@ MESSAGE=$4
 TO_ADDRESS=$5
 SUBJECT=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$MESSAGE/g" -e template.html)
+FINAL_MESSAGE=$(echo $MESSAGE | sed -e 's[&/]/\\&/g')
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE/g" -e template.html)
 
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
-echo "$EMAIL_BODY"
+echo "$FINAL_BODY"
 } | msmtp "$TO_ADDRESS"
