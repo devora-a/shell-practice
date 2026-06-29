4c787127c1592ab0bc1326aed15a7cd5f1aaece0
devora-a <aravinddharla6@gmail.com>
2026-06-29 21:53:36 +0200

    doc.add

--- 10-disk-usage.sh
+++ 10-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=10
+SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read -r line
 do 
@@ -14,3 +15,5 @@ do
 done <<< "$DISK_USAGE"
 
 echo -e "$MESSAGE"
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$SERVER_IP" "$MESSAGE" "aravinddharla6@gmail.com" "High Disk Usage Alert"
