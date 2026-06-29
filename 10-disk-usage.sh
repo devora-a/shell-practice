a7f2e432c784c7ff6bf4216a728f835591185df2
devora-a <aravinddharla6@gmail.com>
2026-06-29 20:11:24 +0200

    doc.add

--- 10-disk-usage.sh
+++ 10-disk-usage.sh
@@ -1,15 +1,15 @@
 #!bin/bash
 
-DISK_USAGE=$(df -hT | grep -v File system)
+DISK_USAGE=$( df -hT | grep -v Filesystem )
 USAGE_THRESHOLD=10
 
 while IFS= read -r line
 do 
-  USAGE=$(echo $line | awk '{print $6 }' | cut -d "%" -f1)
-  PARTITION=$(echo $line | awk '{print $7}')
-    if ["$USAGE" -ge "$USAGE_THRESHOLD"]; then
-      MESSAGE+= "High disk usage on $PARTITION: $USAGE \n"
+  USAGE=$( echo $line | awk '{print $6 }' | cut -d "%" -f1 )
+  PARTITION=$( echo $line | awk '{print $7}' )
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+      MESSAGE+="High disk usage on $PARTITION: $USAGE \n"
     fi
     echo -e "MESSAGE"
 done
-    
+
